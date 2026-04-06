12e9f8f41be0bfd1d6934d3da145ee0fb2d356ab
Václav Slavík <vaclav@slavik.io>
2026-04-06 18:45:34 +0200

    Keep track of devices

--- cec-bridge/mqtt_bridge.py
+++ cec-bridge/mqtt_bridge.py
@@ -10,6 +10,8 @@ import cec
 import msgspec
 
 IDLE_SLEEP = 0.2
+LOGICAL_ADDRESS_COUNT = 15
+BROADCAST_ADDRESS = 15
 
 
 class CECMessage(msgspec.Struct, frozen=True):
@@ -19,6 +21,16 @@ class CECMessage(msgspec.Struct, frozen=True):
     destination: int
     opcode: int | None = None
     parameters: tuple[int, ...] = ()
+
+
+class CECDevice(msgspec.Struct):
+    osd_name: str
+    power_status: str
+    logical_address: int
+    logical_name: str
+    physical_address: str
+    vendor: str
+    cec_version: str
 
 
 def parse_command(raw_command: str) -> CECMessage | None:
@@ -48,9 +60,11 @@ def parse_command(raw_command: str) -> CECMessage | None:
 
 class CECClient:
     def __init__(self) -> None:
+        self.active_source: int | None = None
         self.config: cec.libcec_configuration | None = None
         self.command_callback = self.handle_command
         self.lib: cec.ICECAdapter | None = None
+        self.devices: dict[int, CECDevice] = {}
         self.messages: SimpleQueue[CECMessage] = SimpleQueue()
         self.running = False
 
@@ -84,12 +98,15 @@ class CECClient:
         return 0
 
     def process_message(self, message: CECMessage) -> None:
+        if message.source != BROADCAST_ADDRESS and message.source not in self.devices:
+            self.scan_device(message.source)
+
         opcode = '--' if message.opcode is None else f'{message.opcode:02x}'
-        print(
-            f'dbg src={message.source:x} dst={message.destination:x} opcode={opcode} '
-            f'args={message.parameters} raw="{message.raw}"',
-            flush=True,
-        )
+        #print(
+        #    f'dbg src={message.source:x} dst={message.destination:x} opcode={opcode} '
+        #    f'args={message.parameters} raw="{message.raw}"',
+        #    flush=True,
+        #)
 
     def close(self) -> None:
         if self.lib is not None:
@@ -99,7 +116,37 @@ class CECClient:
         self.running = False
         self.close()
 
+    def scan_devices(self) -> None:
+        self.devices.clear()
+        self.active_source = self.lib.GetActiveSource()
+        for logical_address in range(LOGICAL_ADDRESS_COUNT):
+            self.scan_device(logical_address)
+
+    def scan_device(self, logical_address: int) -> CECDevice | None:
+        if logical_address >= LOGICAL_ADDRESS_COUNT:
+            return None
+        addresses = self.lib.GetActiveDevices()
+        if not addresses.IsSet(logical_address):
+            return None
+
+        pa = self.lib.GetDevicePhysicalAddress(logical_address)
+        physical_nice = f'{(pa >> 12) & 0xF}.{(pa >> 8) & 0xF}.{(pa >> 4) & 0xF}.{pa & 0xF}'
+
+        device = CECDevice(
+            logical_address=logical_address,
+            logical_name=self.lib.LogicalAddressToString(logical_address),
+            physical_address=physical_nice,
+            vendor=self.lib.VendorIdToString(self.lib.GetDeviceVendorId(logical_address)),
+            osd_name=self.lib.GetDeviceOSDName(logical_address),
+            cec_version=self.lib.CecVersionToString(self.lib.GetDeviceCecVersion(logical_address)),
+            power_status=self.lib.PowerStatusToString(self.lib.GetDevicePowerStatus(logical_address)),
+        )
+        print(f'device: {device}')
+        self.devices[logical_address] = device
+        return device
+
     def run(self) -> None:
+        self.scan_devices()
         print('observing...', flush=True)
 
         self.running = True
